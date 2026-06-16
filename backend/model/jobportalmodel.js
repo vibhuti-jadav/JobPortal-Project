89ca12815cd6a35a17e0cc7d299fbb31e5947b5f
vibhuti-jadav <vibhutij109@gmail.com>
2026-06-16 22:08:46 +0530

    schemaa

--- backend/model/jobportalmodel.js
+++ backend/model/jobportalmodel.js
@@ -2,11 +2,28 @@ import mongoose from "mongoose"
 
 const portalSchema = new mongoose.Schema({
 
-    name:{
+    title:{
         type:String,
         required:true,
         trim:true
     },
-    
+    description:{
+        type:String,
+        required:true,
+    },
+    requirments:[String],
+    salary:Number,
+    experienceLevel:Number,
+    location:String,
+    jobType:{
+        type:String,
+        enum:["full-time","part-time","intership","remote"],
+    },
+    position:Number,
+ 
 
 })
+
+const portal = mongoose.model("portal",portalSchema)
+
+export default portal
